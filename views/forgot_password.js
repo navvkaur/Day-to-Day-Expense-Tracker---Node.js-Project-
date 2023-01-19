63b22452cd56d731005ae76aba7ca873ff9ececb
navvkaur <71594057+navvkaur@users.noreply.github.com>
2023-01-19 08:32:11 +0530

    Update forgot_password.js

--- views/forgot_password.js
+++ views/forgot_password.js
@@ -9,7 +9,7 @@ async function reset_Password(event)
     };
     console.log(email);
 
-    const response = await axios.post('http://localhost:3000/forgotpassword',details).then((response)=>{
+    const response = await axios.post('http://43.205.255.229:3000/forgotpassword',details).then((response)=>{
         if(response.status === 202){
             document.getElementById('error').innerHTML += '<div style="color:red;">Mail Successfuly sent <div>'
         } else {
